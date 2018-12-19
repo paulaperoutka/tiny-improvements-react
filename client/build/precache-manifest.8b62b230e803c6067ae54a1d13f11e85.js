051cd4fd3d8046784d3adbb8d1f6047f53e59986
Paula Peroutka <paulaperoutka@gmail.com>
2018-12-19 05:23:55 -0500

    add build iles

---
+++ client/build/precache-manifest.8b62b230e803c6067ae54a1d13f11e85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94e2712742d3a433b7cd",
+    "url": "/static/js/main.94e27127.chunk.js"
+  },
+  {
+    "revision": "be2f66f44b01e0202fa7",
+    "url": "/static/js/1.be2f66f4.chunk.js"
+  },
+  {
+    "revision": "94e2712742d3a433b7cd",
+    "url": "/static/css/main.c66cd45a.chunk.css"
+  },
+  {
+    "revision": "e3b80ff9471bb82a07295255348b2b00",
+    "url": "/index.html"
+  }
+];
